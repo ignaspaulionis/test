6761bc0c6b0da0d2242c747ad4353e76b213189c
Ignas Paulionis <ignaspaulionis@gmail.com>
2023-10-13 14:27:51 +0300

    update

--- api/users.js
+++ api/users.js
@@ -1,19 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can change this to your desired port number
+const port = 3000;
 
-// Middleware to parse JSON data
-app.use(express.json());
-
-// Define a route to serve the contents of response.json
 app.get('/api/users', (req, res) => {
-  // Read the JSON file and send it as a response
   const response = require('./response.json');
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.json(response);
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+module.exports = app;
